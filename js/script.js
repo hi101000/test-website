550913dc724fa147f474887e2b9586f4bdb83954
hi101000 <60079731+hi101000@users.noreply.github.com>
2020-04-29 16:13:21 -0400

    added a password security system

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 function hola(){
 	alert('Im coming for you!')
 }
+<script>
+function myFunction() {
+  var x = document.getElementById("frm1");
+  var text = "";
+  var i;
+  for (i = 0; i < x.length ;i++) {
+    text += x.elements[i].value + "<br>";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
+</script>
