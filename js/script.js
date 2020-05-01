2884c1034d6ae6deb0f784c81af841a041b43fa6
hi101000 <60079731+hi101000@users.noreply.github.com>
2020-05-01 12:18:04 -0400

    fixed it

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
 function hola(){
 	alert('Im coming for you!')
 }
-<script>
-function myFunction() {
-  var x = document.getElementById("frm1");
-  var text = "";
-  var i;
-  for (i = 0; i < x.length ;i++) {
-    text += x.elements[i].value + "<br>";
-  }
-  if (text!=Ishan Roychowdhury){
-  	confirm(is this what you want to submit?)
-  	document.write('Haha! nice try theif')
-  	window.close()
-  }
-  document.getElementById("demo").innerHTML = text;
-}
-</script>
