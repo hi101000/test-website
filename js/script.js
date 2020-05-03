1d2af5838318f28790dab4bef33d550d1609d1f3
pipan <piklenews@gmail.com>
2020-05-03 15:45:52 -0400

    added form validation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 function hola(){
 	alert("Hi!")
 }
-
+function validateForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x=='Ishan'){
+  	window.close
+  }
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
