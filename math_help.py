1eb6542f856b0f12059af58bb19119d3dd9a9dce
hi101000 <60079731+hi101000@users.noreply.github.com>
2020-05-05 19:48:16 -0400

    added some more functions

--- math_help.py
+++ math_help.py
@@ -10,3 +10,16 @@ def absval(x):
 def average(x, y):
     z=x+y/2
     return z
+    
+
+def average_2ormore(*values):
+    average=0
+    for x in values:
+        average+=x
+    average/=2
+    return average
+    
+
+def make_absval_equation(answer1, answer2):
+    average(answer1, answer2)
+    
