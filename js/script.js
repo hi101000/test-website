57897e9824b865d5839f1e5cfc3b7d20d0fb4d6e
hi101000 <60079731+hi101000@users.noreply.github.com>
2020-05-06 11:33:25 -0400

    made it smaller

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
 function hola(){
 	alert("Hi!");
 }
-function validateForm() {
-  var x = document.forms["myForm"]["fname"].value;
-  document.cookie='username=x; expires=expires=Fri, 01 Jan 2021 00:00:00 UTC;';
-  if (x=='Ishan'){
-  	window.close();
-  }
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-}
-/*function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  var expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}*/
